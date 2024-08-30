5258a2a13e5f59b7c36b72cb9e6624c4b368ec79
hacksever <harbhajansingh7852@gmail.com>
2024-08-30 14:35:34 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY KaliaYt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7400337646:AAG58cKuTq4gJcp0Q7n7sfPFJt9yW5cpsAI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("KaliaYtOwner")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6862918172")
+
+
+
+
+
+
+
+  
